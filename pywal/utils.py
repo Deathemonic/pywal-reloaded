6b2e3526913f0ea1197045c37d4b8794064f3d3d
ヒヤシンス <deathemonic@gmail.com>
2023-01-28 22:00:56 +0800

    rewrite: utils.py
    - fixed some color convertion
    - added rich logger
    - removed redundant code
    - removed old code
    - refactored some code
    - added more optimization
    
    TODO:
    - add docstrings
    - remove unneeded properties
    - add remaining type hints

--- pywal/utils.py
+++ pywal/utils.py
@@ -1,199 +1,129 @@
 """
-Misc helper functions.
+Helper functions and classes.
 """
+
 import colorsys
-import json
 import logging
-import os
-import platform
-import re
-import shutil
+import numpy as np
+import struct
 import subprocess
-import sys
+import os
+
+from json import load, dump
+from pathlib import PosixPath, Path
+from platform import system
+from shutil import which
+from sys import stdout
 
 
 class Color:
-    """ Color formats. """
+    alpha_num = '100'
     
-    alpha_num = "100"
-
-    def __init__(self, hex_color):
+    def __init__(self, hex_color: str) -> None:
         self.hex_color = hex_color
-
-    def __str__(self):
+        
+    def __str__(self) -> str:
         return self.hex_color
-
+    
     @property
-    def rgb(self):
-        """ Convert a hex color to rgb. """
-        return f"{hex_to_rgb(self.hex_color)[0]}," \
-               f"{hex_to_rgb(self.hex_color)[1]}," \
-               f"{hex_to_rgb(self.hex_color)[2]}"
-
+    def rgb(self) -> str:
+        return ','.join(map(str, hex_to_rgb(self.hex_color)))
+    
     @property
-    def xrgba(self):
-        """ Convert a hex color to xrdb rgba. """
+    def xrgba(self) -> str:
         return hex_to_xrgba(self.hex_color)
-
+    
     @property
-    def rgba(self):
-        """ Convert a hex color to rgba. """
-        return f"rgba({hex_to_rgb(self.hex_color)[0]}," \
-               f"{hex_to_rgb(self.hex_color)[1]}," \
-               f"{hex_to_rgb(self.hex_color)[2]}," \
-               f"{self.alpha_dec})"
-
+    def rgba(self) -> str:
+        r, g, b = hex_to_rgb(self.hex_color)
+        return f'rgba({r}, {g}, {b}, {self.alpha_dec})'
+    
     @property
-    def alpha(self):
-        """ Add URxvt alpha value to color. """
-        return f"[{self.alpha_num}]{self.hex_color}"
-
+    def alpha(self) -> str:
+        return f"[{self.alpha_num}]{self.hex_color}"            
+    
     @property
-    def alpha_dec(self):
-        """Export the alpha value as a decimal number in [0, 1]."""
+    def alpha_dec(self) -> float:
         return int(self.alpha_num) / 100
 
     @property
-    def decimal(self):
-        """Export color in decimal."""
-        return f"#{int(self.hex_color[1:], 16)}"
+    def decimal(self) -> str:
+        return f"#{int(self.hex_color.strip('#'), 16)}"
+    
+    @property
+    def decimal_strip(self) -> float:
+        return int(self.hex_color.strip('#'), 16)
+    
+    @property
+    def octal(self) -> str:
+        return f"#{oct(int(self.hex_color.strip('#'), 16))[2:]}"
 
     @property
-    def decimal_strip(self):
-        """ Strip '#' from decimal color. """
-        return int(self.hex_color[1:], 16)
+    def octal_strip(self) -> str:
+        return oct(int(self.hex_color.strip('#'), 16))[2:]
+    
+    @property
+    def strip(self) -> str:
+        return self.hex_color.strip('#')
+    
+    @property
+    def red(self) -> str:
+        return f'{hex_to_rgb(self.hex_color)[0]/255.:.3f}'
 
     @property
-    def octal(self):
-        """ Export color in octal. """
-        return f"#{oct(int(self.hex_color[1:], 16))[2:]}"
+    def green(self) -> str:
+        return f'{hex_to_rgb(self.hex_color)[1]/255.:.3f}'
 
     @property
-    def octal_strip(self):
-        """ Strip '#' from octal color. """
-        return oct(int(self.hex_color[1:], 16))[2:]
-
-    @property
-    def strip(self):
-        """ Strip '#' from color. """
-        return self.hex_color[1:]
-
-    @property
-    def red(self):
-        """ Red value as float between 0 and 1. """
-        return f"{hex_to_rgb(self.hex_color)[0]/255.:.3f}"
-
-    @property
-    def green(self):
-        """ Green value as float between 0 and 1. """
-        return f"{hex_to_rgb(self.hex_color)[1]/255.:.3f}"
-
-    @property
-    def blue(self):
-        """ Blue value as float between 0 and 1. """
-        return f"{hex_to_rgb(self.hex_color)[3]/255.:.3f}"
-
-    def lighten(self, percent):
-        """ Lighten color by percent. """
-        percent = float(re.sub(r'[\D\.]', '', str(percent)))
-        return Color(lighten_color(self.hex_color, percent / 100))
-
-    def darken(self, percent):
-        """Darken color by percent."""
-        percent = float(re.sub(r'[\D\.]', '', str(percent)))
-        return Color(darken_color(self.hex_color, percent / 100))
-
-    def saturate(self, percent):
-        """Saturate a color."""
-        percent = float(re.sub(r'[\D\.]', '', str(percent)))
-        return Color(saturate_color(self.hex_color, percent / 100))
+    def blue(self) -> str:
+        return f'{hex_to_rgb(self.hex_color)[3]/255.:.3f}'
+    
+    def darken(self, percent: int | float):
+        percent = int(percent * 100) if type(percent) is float else percent
+        return Color(brighten_color(self.hex_color, percent, False))
+    
+    def lighten(self, percent: int | float):
+        percent = int(percent * 100) if type(percent) is float else percent
+        return Color(brighten_color(self.hex_color, percent, True))
+        
+    def saturate(self, percent: int | float):
+        percent = int(percent * 100) if type(percent) is float else percent
+        return Color(saturate_color(self.hex_color, percent))
 
 
-def read_file(input_file):
-    """Read data from a file and trim newlines."""
-    with open(input_file, "r") as file:
-        return file.read().splitlines()
+def hex_to_rgb(color: str) -> tuple:
+    return struct.unpack('BBB', bytes.fromhex(color.strip('#')))
 
 
-def read_file_json(input_file):
-    """Read data from a json file."""
-    with open(input_file, "r") as json_file:
-        return json.load(json_file)
+def hex_to_xrgba(color: str) -> str:
+    col = color.lower().strip('#')
+    return f'{col[:2]}/{col[2:4]}/{col[4:6]}/ff'
 
 
-def read_file_raw(input_file):
-    """Read data from a file as is, don't strip
-       newlines or other special characters."""
-    with open(input_file, "r") as file:
-        return file.readlines()
+def hex_to_yiq(color: str) -> tuple:
+    r, g, b = hex_to_rgb(color)
+    y, i, q = colorsys.rgb_to_yiq(r, g, b)
+    return np.around(y, decimals=2), np.around(i, decimals=2), np.around(q, decimals=2)
 
 
-def save_file(data, export_file):
-    """Write data to a file."""
-    create_dir(os.path.dirname(export_file))
-
-    try:
-        with open(export_file, "w") as file:
-            file.write(data)
-    except PermissionError:
-        logging.warning("Couldn't write to %s.", export_file)
+def rgb_to_hex(color: tuple) -> str:
+    return '#' + ''.join(f'{x:02x}' for x in color)
 
 
-def save_file_json(data, export_file):
-    """Write data to a json file."""
-    create_dir(os.path.dirname(export_file))
-
-    with open(export_file, "w") as file:
-        json.dump(data, file, indent=4)
-
-
-def create_dir(directory):
-    """Alias to create the cache dir."""
-    os.makedirs(directory, exist_ok=True)
-
-
-def setup_logging():
-    """Logging config."""
-    logging.basicConfig(format=("[%(levelname)s\033[0m] "
-                                "\033[1;31m%(module)s\033[0m: "
-                                "%(message)s"),
-                        level=logging.INFO,
-                        stream=sys.stdout)
-    logging.addLevelName(logging.ERROR, '\033[1;31mE')
-    logging.addLevelName(logging.INFO, '\033[1;32mI')
-    logging.addLevelName(logging.WARNING, '\033[1;33mW')
-
-
-def hex_to_rgb(color):
-    """ Convert a hex color to rgb. """
-    return tuple(bytes.fromhex(color.strip("#")))
-
-
-def hex_to_xrgba(color):
-    """ Convert a hex color to xrdb rgba. """
-    col = color.lower().strip("#")
-    return f"{col[:2]}/{col[2:4]}/{col[4:6]}/ff"
-
-
-def rgb_to_hex(color):
-    """ Convert a rgb color to hex. """
-    return f"#{color[0]:02x}{color[1]:02x}{color[2]:02x}"
-
-
-def darken_color(color, amount):
-    """Darken a hex color."""
-    color = [int(col * (1 - amount)) for col in hex_to_rgb(color)]
+def brighten_color(hex: str, amount: float, brighten: bool) -> str:
+    color = tuple(min(255, col + amount) if brighten else max(1, col - amount) for col in hex_to_rgb(hex))
     return rgb_to_hex(color)
 
 
-def lighten_color(color, amount):
-    """Lighten a hex color."""
-    color = [int(col + (255 - col) * amount) for col in hex_to_rgb(color)]
-    return rgb_to_hex(color)
+def saturate_color(color: str, amount: float) -> str:
+    r, g, b = hex_to_rgb(color)
+    h, l, s = colorsys.rgb_to_hls(r/255, g/255, b/255)
+    s = amount / 100
+    r, g, b = colorsys.hls_to_rgb(h, l, s)
+    return rgb_to_hex((int(r*255), int(g*255), int(b*255)))
 
 
-def blend_color(color, color2):
-    """Blend two colors together."""
+def blend_color(color: str, color2: str) -> str:
     r1, g1, b1 = hex_to_rgb(color)
     r2, g2, b2 = hex_to_rgb(color2)
 
@@ -204,43 +134,68 @@ def blend_color(color, color2):
     return rgb_to_hex((r3, g3, b3))
 
 
-def saturate_color(color, amount):
-    """Saturate a hex color."""
-    r, g, b = hex_to_rgb(color)
-    r, g, b = [x / 255.0 for x in (r, g, b)]
-    h, l, s = colorsys.rgb_to_hls(r, g, b)
-    s = amount
-    r, g, b = colorsys.hls_to_rgb(h, l, s)
-    r, g, b = [x * 255.0 for x in (r, g, b)]
-
-    return rgb_to_hex((int(r), int(g), int(b)))
-
-
-def rgb_to_yiq(color):
-    """Sort a list of colors."""
-    return colorsys.rgb_to_yiq(*hex_to_rgb(color))
+def setup_logging():
+    try:
+        from rich.logging import RichHandler
+        logging.basicConfig(format='%(message)s', level=logging.INFO, datefmt='[%X]', handlers=[RichHandler(rich_tracebacks=True)])
+        logging.getLogger('rich')
+    except ImportError:
+        format = '[%(levelname)s\033[0m] \033[1;31m%(module)s\033[0m: %(message)s'
+        logging.basicConfig(format=format, level=logging.INFO, stream=stdout)
+        logging.addLevelName(logging.ERROR, '\033[1;31mE')
+        logging.addLevelName(logging.INFO, '\033[1;32mI')
+        logging.addLevelName(logging.WARNING, '\033[1;33mW')
 
 
-def disown(cmd):
-    """Call a system command in the background,
-       disown it and hide it's output."""
-    subprocess.Popen(cmd,
-                     stdout=subprocess.DEVNULL,
-                     stderr=subprocess.DEVNULL)
+def create_dir(path: Path | str):
+   PosixPath(path).mkdir(parents=True, exist_ok=True) 
 
 
-def get_pid(name):
-    """Check if process is running by name."""
-    if not shutil.which("pidof"):
+def read_file(input_file):
+    with open(input_file, 'r') as file:
+        return file.read().splitlines()
+
+
+def read_file_json(input_file):
+    with open(input_file, 'r') as json_file:
+        return load(json_file)
+
+
+def read_file_raw(input_file):
+    with open(input_file, 'r') as file:
+        return file.readlines()
+
+
+def save_file(data, export_file):
+    create_dir(os.path.dirname(export_file))
+
+    try:
+        with open(export_file, 'w') as file:
+            file.write(data)
+    except PermissionError:
+        logging.warning(f"Couldn't write to {export_file}")
+
+
+def save_file_json(data, export_file):
+    create_dir(os.path.dirname(export_file))
+    with open(export_file, "w") as file:
+        dump(data, file, indent=4)
+
+
+def disown(cmd: str):
+    subprocess.Popen(
+        cmd,
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL
+    )
+
+
+def get_pid(name: str) -> bool:
+    if not which("pidof"):
         return False
 
     try:
-        if platform.system() != 'Darwin':
-            subprocess.check_output(["pidof", "-s", name])
-        else:
-            subprocess.check_output(["pidof", name])
-
+        subprocess.check_output(["pidof", "-s", name]) if system() != 'Darwin' else subprocess.check_output(['pidof', name])
     except subprocess.CalledProcessError:
         return False
-
     return True
